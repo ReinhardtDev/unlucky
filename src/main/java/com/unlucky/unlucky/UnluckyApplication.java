af7ebdb9d5e55eb6d14cec525b2781fb92a63ea1
Oliver <oliverreinhardt@outlook.de>
2025-08-12 12:33:17 +0200

    spring boot set up

---
+++ src/main/java/com/unlucky/unlucky/UnluckyApplication.java
@@ -0,0 +1,13 @@
+package com.unlucky.unlucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnluckyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnluckyApplication.class, args);
+	}
+
+}
