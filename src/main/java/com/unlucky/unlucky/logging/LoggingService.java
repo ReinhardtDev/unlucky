bdb092c573e3ace44301dad566e660abe3ec18e8
Lukas <streubel.lukas@gmail.com>
2025-08-26 21:35:41 +0200

    added support for total time and stage logging

--- src/main/java/com/unlucky/unlucky/logging/LoggingService.java
+++ src/main/java/com/unlucky/unlucky/logging/LoggingService.java
@@ -18,6 +18,7 @@ public class LoggingService {
     public enum ACTION {
         REGISTER_USER,
         AVERAGE_TIME,
+        TOTAL_TIME,
         DRAW_CLASSIC,
         DRAW_649,
         PURCHASE_TICKET_CLASSIC,
@@ -30,23 +31,58 @@ public class LoggingService {
     public void log(ACTION action, double time, String... userParams) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(logFile, true))) {
             String timeStamp = LocalDateTime.now().format(formatter);
-            if (action != ACTION.AVERAGE_TIME && action != ACTION.PURCHASE_TICKET_CLASSIC && action != ACTION.PURCHASE_TICKET_649) {
-                writer.println("[" + timeStamp + "] " + action + " in " + time + "ms");
-                writer.flush();
-            } else if (action == ACTION.AVERAGE_TIME) {
-                writer.println("[" + timeStamp + "] " + action + ": " + time + "ms");
-            } else {
-                writer.println("[" + timeStamp + "] " +
-                        action + " by user: " +
-                        userParams[0] + " with quantity " +
-                        userParams[1] + " in " +
-                        time + "ms");
+
+            switch (action) {
+                case REGISTER_USER:
+                    writer.println("[" + timeStamp + "] " + action + " with name: " + userParams[0] + ", in " + time + "ms");
+                    writer.flush();
+                    break;
+
+                case AVERAGE_TIME:
+                    writer.println("[" + timeStamp + "] " + action + ": " + time + "ms per request");
+                    break;
+
+                case TOTAL_TIME:
+                    writer.println("[" + timeStamp + "] " + action + ": " + time + "ms");
+                    break;
+
+                case PURCHASE_TICKET_CLASSIC:
+                    writer.println("[" + timeStamp + "] " +
+                            action + " by user: " +
+                            userParams[0] + ", with quantity " +
+                            userParams[1] + " in " +
+                            time + "ms");
+                    break;
+
+                case PURCHASE_TICKET_649:
+                    writer.println("[" + timeStamp + "] " +
+                            action + " by user: " +
+                            userParams[0] + ", with numbers " +
+                            userParams[1] + " in " +
+                            time + "ms");
+                    break;
+
+                default:
+                    writer.println("[" + timeStamp + "] " + action + " in " + time + "ms");
+                    break;
             }
 
-
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error writing to log file", e);
+        }
+    }
+
+    public void logStage(String stage, boolean nextStage) {
+        try(PrintWriter writer = new PrintWriter(new FileWriter(logFile, true))) {
+            String timeStamp = LocalDateTime.now().format(formatter);
+            writer.println("[-------------------------------" + timeStamp + "-------------------------------]");
+            writer.println("[" + timeStamp + "] " + stage);
+            if (nextStage) {
+                writer.println("Starting next stage...");
+                writer.println("---------------------------------------------------------------------------------------");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing stage to log file", e);
         }
     }
 }
