ae2fd4c82c05a161ee87e50fa1971113f06b2436
Lukas <streubel.lukas@gmail.com>
2025-08-26 14:31:13 +0200

    added support for drawing and purchasing tickets

--- src/main/java/com/unlucky/unlucky/logging/LoggingService.java
+++ src/main/java/com/unlucky/unlucky/logging/LoggingService.java
@@ -1,5 +1,7 @@
 package com.unlucky.unlucky.logging;
 
+import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Service
 public class LoggingService {
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
@@ -15,20 +18,29 @@ public class LoggingService {
     public enum ACTION {
         REGISTER_USER,
         AVERAGE_TIME,
-        PURCHASE
+        DRAW_CLASSIC,
+        DRAW_649,
+        PURCHASE_TICKET_CLASSIC,
+        PURCHASE_TICKET_649
     }
 
     public LoggingService() {
     }
 
-    public void log(ACTION action, double... time) {
+    public void log(ACTION action, double time, String... userParams) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(logFile, true))) {
             String timeStamp = LocalDateTime.now().format(formatter);
-            if (action != ACTION.AVERAGE_TIME) {
-                writer.println("[" + timeStamp + "] " + action.toString() + " in " + time[0] + "ms");
+            if (action != ACTION.AVERAGE_TIME && action != ACTION.PURCHASE_TICKET_CLASSIC && action != ACTION.PURCHASE_TICKET_649) {
+                writer.println("[" + timeStamp + "] " + action + " in " + time + "ms");
                 writer.flush();
+            } else if (action == ACTION.AVERAGE_TIME) {
+                writer.println("[" + timeStamp + "] " + action + ": " + time + "ms");
             } else {
-                writer.println("[" + timeStamp + "] " + action + ": " + time[0] + "ms");
+                writer.println("[" + timeStamp + "] " +
+                        action + " by user: " +
+                        userParams[0] + " with quantity " +
+                        userParams[1] + " in " +
+                        time + "ms");
             }
 
 
